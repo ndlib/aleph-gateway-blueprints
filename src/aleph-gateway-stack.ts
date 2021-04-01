639cb154e05046fb2023553a62c9b162dc9b178b
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2021-04-01 15:09:53 -0400

    Consolidate roles.

--- src/aleph-gateway-stack.ts
+++ src/aleph-gateway-stack.ts
@@ -5,6 +5,7 @@ import lambda = require('@aws-cdk/aws-lambda')
 import { RetentionDays } from '@aws-cdk/aws-logs'
 import { StringParameter } from '@aws-cdk/aws-ssm'
 import { Vpc, SecurityGroup, Subnet } from '@aws-cdk/aws-ec2'
+import { Role, ServicePrincipal, ManagedPolicy } from '@aws-cdk/aws-iam'
 
 export interface IAlephGatewayStackProps extends cdk.StackProps {
   readonly stage: string
@@ -37,6 +38,16 @@ export default class AlephGatewayStack extends cdk.Stack {
       ALEPH_ORACLE_PORT: SecretValue.secretsManager(props.secretsPath, { jsonField: 'db_port' }).toString(),
       ALEPH_ORACLE_SID: SecretValue.secretsManager(props.secretsPath, { jsonField: 'db_sid' }).toString(),
     }
+
+    // Role for lambda Execution. If we don't create this, cdk will make one for EACH lambda
+    const lambdaExecutionRole = new Role(this, 'LambdaRole', {
+      roleName: `${this.stackName}-lambdaExecutionRole`,
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      managedPolicies: [
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaBasicExecutionRole'),
+        ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSLambdaVPCAccessExecutionRole'),
+      ],
+    })
 
     // VPC needed to access certain APIs.
     const vpcId = Fn.importValue(`${props.networkStackName}:VPCID`)
@@ -81,6 +92,8 @@ export default class AlephGatewayStack extends cdk.Stack {
         subnets: [subnet],
       },
       securityGroups: [securityGroup],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const pendingLambda = new lambda.Function(this, 'PendingFunction', {
@@ -98,6 +111,8 @@ export default class AlephGatewayStack extends cdk.Stack {
         subnets: [subnet],
       },
       securityGroups: [securityGroup],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const itemLambda = new lambda.Function(this, 'ItemFunction', {
@@ -115,6 +130,8 @@ export default class AlephGatewayStack extends cdk.Stack {
         subnets: [subnet],
       },
       securityGroups: [securityGroup],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const renewLambda = new lambda.Function(this, 'RenewFunction', {
@@ -132,6 +149,8 @@ export default class AlephGatewayStack extends cdk.Stack {
         subnets: [subnet],
       },
       securityGroups: [securityGroup],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const userInfoLambda = new lambda.Function(this, 'UserInfoFunction', {
@@ -153,6 +172,8 @@ export default class AlephGatewayStack extends cdk.Stack {
       },
       securityGroups: [securityGroup],
       layers: [oracleLayer],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const circHistoryLambda = new lambda.Function(this, 'CircHistoryFunction', {
@@ -174,6 +195,8 @@ export default class AlephGatewayStack extends cdk.Stack {
       },
       securityGroups: [securityGroup],
       layers: [oracleLayer],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     const queryLambda = new lambda.Function(this, 'QueryFunction', {
@@ -193,6 +216,8 @@ export default class AlephGatewayStack extends cdk.Stack {
         subnets: [subnet],
       },
       securityGroups: [securityGroup],
+      role: lambdaExecutionRole,
+      logRetentionRole: lambdaExecutionRole,
     })
 
     // API GATEWAY
